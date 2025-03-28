6eb23b8da1b675cb9216c8d4bf74747714fc7d22
Lesya Kudryashova <lkudryashova.work@gmail.com>
2025-03-28 16:37:14 +0100

    HW01 update

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,9 @@
-export const PATH_DB =
+import path from 'node:path';
+
+/*export const PATH_DB = path.join(
+  process.cwd(),
+  'src',
+  'db',
+  'db-contacts.json'
+);*/
+export const PATH_DB = path.resolve('src', 'db', 'db.json');
--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,11 @@
+import fs from 'node:fs/promises';
+
 import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  //const buffer = await fs.readFile(PATH_DB);
+  //const text = buffer.toString();
+  //console.log(text);
+  const data = await fs.readFile(PATH_DB, 'utf-8');
+  return JSON.parse(data);
+};
